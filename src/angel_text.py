ee7b815593d48f450996ca039d0dcdfa6bbba096
Tristan Tran <tristntran@gmail.com>
2022-05-13 14:10:43 -0700

    updating code

--- src/angel_text.py
+++ src/angel_text.py
@@ -1,21 +1,71 @@
+import random
 from twilio.rest import Client
+import json
+from os import listdir
+from os.path import isfile, join
 
 class AngelText:
-    def __init__(self,sid,token,number):
+    def __init__(self,sid,token,number,cellphone):
         self.sid = sid
         self.token = token
         self.number = number
+        self.cellphone = cellphone
         self.client = Client(self.sid, self.token)
-        
-    def send_message(self, msg: str, cellphone: str):
+        self.angel_times = {'1:11': [],
+                            '2:22': [],
+                            '3:33': [],
+                            '4:44': [],
+                            '5:55': [],
+                            '10:10': [],
+                            '11:11': [],
+                            '12:12': [],
+                            '12:34': [],
+                            '13:11': [],
+                            '14:22': [],
+                            '15:33': [],
+                            '16:44': [],
+                            '17:55': [],
+                            '22:10': [],
+                            '23:11': []
+                           }
+    
+    def init_angel_times(self,source_path):
+        onlyfiles = [f for f in listdir(source_path) if isfile(join(source_path, f))]
+        for file in onlyfiles:
+            key = self.get_key(file)
+            self.read_messages(key,file)
+
+    def get_key(filename)-> str:
+        key_start = filename.split('.')
+        return key_start[:-2] + ':' + key_start[-2:]
+ 
+    def read_messages(self,key,filepath):
+        with open(filepath) as f:
+            self.angel_times[key] = json.load(f)
+
+    def send_message(self, msg: str):
         """
         sends a designate message to a phone number
 
         :param msg: message to send
         :param cellphone: string of cell number for recipient
         """
-        self.client.messages.create(to=cellphone,
+        self.client.messages.create(to=self.cellphone,
                             from_=self.number,
                             body=msg
                             )
 
+    def send_angel_message(self,time):
+        msg = random.choice(self.angel_times[time])
+        if msg:
+            self.send_message(msg)
+
+
+    def set_message(self):
+        times = ['1:11','2:22','3:33','4:44','5:55','10:10',
+                 '11:11','12:12','12:34','13:11','14:22',
+                 '15:33','16:44','17:55','22:10','23:11'
+                ]
+        if not random.randint(0,7):
+            time = random.choice(times)
+            self.send_angel_message(time)
--- src/main.py
+++ src/main.py
@@ -10,23 +10,16 @@ with open("./src/twilio-security.json") as f:
     twilio_account= twilio_credentials["sid"]
     twilio_token = twilio_credentials["token"]
     twilio_number = twilio_credentials["phone_number"]
-texter = angel_text.AngelText(twilio_account, twilio_token, twilio_number)
-msg = "Testing Hi Brother - Tristan"
-phone = "14082052148"
-texter.send_message(msg,phone)
-# # send a message in the morning
-# schedule.every().day.at("1:11").do(send_message, GOOD_MORNING_QUOTES)
-# schedule.every().day.at("11:11").do(send_message, GOOD_MORNING_QUOTES)
-# schedule.every().day.at("2:22").do(send_message, GOOD_MORNING_QUOTES)
-# schedule.every().day.at("3:33").do(send_message, GOOD_MORNING_QUOTES)
-# schedule.every().day.at("4:44").do(send_message, GOOD_MORNING_QUOTES)
-# schedule.every().day.at("5:55").do(send_message, GOOD_MORNING_QUOTES)
-# schedule.every().day.at("1:23").do(send_message, GOOD_MORNING_QUOTES)
-# schedule.every().day.at("12:12").do(send_message, GOOD_MORNING_QUOTES)
-# schedule.every().day.at("12:34").do(send_message, GOOD_MORNING_QUOTES)
+phone = "13036188166"
+texter = angel_text.AngelText(twilio_account, twilio_token, twilio_number,phone)
+msg = "Hi Arianna. This is your angel speaking."
 
-# while True:
-#     # Checks whether a scheduled task
-#     # is pending to run or not
-#     schedule.run_pending()
-#     time.sleep(2)
+# texter.send_message(msg,phone)
+# # send a message in the morning
+schedule.every().day.at("00:00").do(texter.set_message)
+
+while True:
+    # Checks whether a scheduled task
+    # is pending to run or not
+    schedule.run_pending()
+    time.sleep(2)
